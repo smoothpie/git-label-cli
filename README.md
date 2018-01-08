9bbbc7d147f32f40d6b3ea8b85e159426fa21013
Kirill Volkovich <kirill.volkovich@opuscapita.com>
2018-01-08 20:50:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# git-label-cli
+
+Create and remove labels across GitHub repositories
+
+## Installation
+
+`npm install -g git-label-cli`
+
+## Examples:
+
+`./run.sh -- opuscapita/git-label-cli`
+`./run.sh --repositories-json`
+
+**repositories.json** example:
+
+```
+[
+  "github_user/repo-1",
+  "github_user/repo-2",
+  "github_user/repo-3"
+]
+```
+
+**labels-to-create.json** example:
+
+```
+[
+  { "name": "closed: completed", "color": "#d93f0b" },
+  { "name": "closed: duplicate", "color": "#d93f0b" },
+  { "name": "closed: wontfix", "color": "#d93f0b" },
+  { "name": "requested by: myFavoriteCustomer", "color": "#0052cc" },
+  { "name": "type: bug", "color": "#1d76db" },
+  { "name": "type: enhancement", "color": "#1d76db" }
+]
+```
+
+**labels-to-remove.json** example:
+
+```
+[
+  { "name": "bug" },
+  { "name": "enhancement" },
+  { "name": "duplicate" },
+  { "name": "good first issue" },
+  { "name": "help wanted" },
+  { "name": "invalid" },
+  { "name": "question" },
+  { "name": "wontfix" }
+]
+```
+
+## Related projects
+
+[https://github.com/jasonbellamy/git-label](git-label)
+
+## License
+
+Licensed under the Apache License, Version 2.0. See [LICENSE](./LICENSE) for the full license text.
---
+++ index.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+let path = require('path');
+let chalk = require('chalk');
+let packageInfo = require('./package.json');
+let gitLabel = require('git-label');
+
+let labelsToCreatePath = path.join(process.cwd(), `./labels-to-create.json`);
+let labelsToCreate = fs.existsSync(labelsToCreatePath) ? require(labelsToCreatePath) : [];
+
+let labelsToRemovePath = path.join(process.cwd(), './labels-to-remove.json');
+let labelsToRemove = fs.existsSync(labelsToRemovePath) ? require(labelsToRemovePath) : [];
+
+let log = msg => console.log(msg);
+let logDanger = msg => console.log(chalk.red.bold(msg));
+let logInfo = msg => console.log(chalk.blue.bold(msg));
+
+if (!process.env.GITHUB_TOKEN) {
+  log(`$GITHUB_TOKEN env variable MUST be provided`);
+  process.exit(1);
+}
+
+if (
+  process.argv.indexOf('--help') !== -1 ||
+  process.argv.indexOf('-h') !== -1 ||
+  process.argv.length <= 2
+) {
+  log(`${packageInfo.name} version: ${packageInfo.version}`);
+  log(`Synopsis: create and remove labels across GitHub repositories\n`);
+
+  logDanger(`----------------------------------------------------------------`);
+  logDanger(`Be careful! Use it only for new repository or`);
+  logDanger(`repositories with a little count of issues`);
+  logDanger(`Existing labels applied to issues can be lost`);
+  logDanger(`Label replacement instead of deleting will be added (maybe...)`);
+  logDanger(`----------------------------------------------------------------\n`);
+
+  logInfo(`$GITHUB_TOKEN env variable MUST be provided!`);
+  logInfo(`'labels-to-add.json' and 'labels-to-remove.json' **SHOULD** be in CWD\n`);
+  logInfo(`json:`);
+  log(`[ { "name": "closed: completed", "color": "#d93f0b" }, ... ]\n`);
+
+  logInfo(`Examples:`);
+  log(`$ ./run.sh -- opuscapita/minsk-core-sandbox opuscapita/test-circleci-release-process`);
+  log(`$ ./run.sh --repositories-json\n`);
+
+  log(`Issues: https://github.com/OpusCapita/git-label-cli/issues\n\n`);
+
+  process.exit(0);
+}
+
+let repositoriesSelectionMethod = process.argv.indexOf('--repositories-json') !== -1 ? 'FROM_FILE' : 'FROM_ARGV';
+let repositories = repositoriesSelectionMethod === 'FROM_FILE' ?
+    require(path.join(process.cwd(), './repositories.json')) :
+    process.argv.slice(2, process.argv.length);
+
+function applyLabels({ api, repository, token, labelsToCreate, labelsToRemove }) {
+  let config = {
+	  api,
+	  repo: repository,
+	  token
+  };
+
+  gitLabel.remove(config, labelsToRemove)
+	  .then(console.log)
+	  .catch(console.log);
+
+  gitLabel.add(config, labelsToCreate)
+	  .then(console.log)
+	  .catch(console.log);
+}
+
+repositories.forEach(repository => {
+  applyLabels({
+	  api: 'https://api.github.com',
+	  repository,
+	  token: process.env.GITHUB_TOKEN,
+	  labelsToCreate,
+	  labelsToRemove
+  });
+});
